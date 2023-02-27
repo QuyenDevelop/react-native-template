e85a57e07dca93f8859825d7cdd5fbe8687c7e57
Phạm Quyền <quyenph@ichiba.vn>
2023-02-27 16:17:27 +0700

    config axios

---
+++ src/api/authApi.ts
@@ -0,0 +1,84 @@
+import { Account } from "@models";
+import Config from "react-native-config";
+import { BaseApi } from "./baseApi";
+
+const {
+  IDENTITY_HOST,
+  GRANT_TYPE_PASSWORD,
+  GRANT_TYPE_EXTERNAL,
+  GRANT_TYPE_REFRESH_TOKEN,
+  SCOPES,
+  CLIENT_ID,
+  CLIENT_SECRET,
+} = Config;
+class AuthApi extends BaseApi {
+  login = async (email?: string, password?: string) => {
+    let url = `${IDENTITY_HOST}/connect/token`;
+    return await this.postUrlEncoded(
+      url,
+      {
+        username: email,
+        password: password,
+        grant_type: `${GRANT_TYPE_PASSWORD}`,
+        client_secret: `${CLIENT_SECRET}`,
+        scope: `${SCOPES}`,
+        client_id: `${CLIENT_ID}`,
+      },
+      true
+    );
+  };
+
+  loginExternal = async (token: string, provider: string) => {
+    let url = `${IDENTITY_HOST}/connect/token`;
+    return this.postUrlEncoded(
+      url,
+      {
+        token: token,
+        provider: provider,
+        grant_type: `${GRANT_TYPE_EXTERNAL}`,
+        client_secret: `${CLIENT_SECRET}`,
+        scope: `${SCOPES}`,
+        client_id: `${CLIENT_ID}`,
+      },
+      true
+    );
+  };
+
+  refreshToken(refreshToken: string) {
+    let url = `${IDENTITY_HOST}/connect/token`;
+    return this.postUrlEncoded(
+      url,
+      {
+        refresh_token: refreshToken,
+        grant_type: `${GRANT_TYPE_REFRESH_TOKEN}`,
+        client_secret: `${CLIENT_SECRET}`,
+        scope: `${SCOPES}`,
+        client_id: `${CLIENT_ID}`,
+      },
+      true
+    );
+  }
+
+  revokeToken(token?: string | null) {
+    let url = `${IDENTITY_HOST}/connect/revocation`;
+    return this.postUrlEncoded(
+      url,
+      {
+        token,
+      },
+      true
+    );
+  }
+
+  getUserInfo = async () => {
+    let url = `${IDENTITY_HOST}/connect/userinfo`;
+    return await this.get<Account>(url, {}, true);
+  };
+
+  // updateRegion(countryCode: string) {
+  //   let url = `${IDENTITY_HOST}/api/profile/updateregion`;
+  //   return this.post(url, { countryCode }, {}, true);
+  // }
+}
+
+export default new AuthApi();
---
+++ src/api/index.tsx
@@ -0,0 +1,11 @@
+// import accountApi from "./account-api";
+import authApi from "./authApi";
+// import commonApi from "./common-api";
+// import customerApi from "./customer-api";
+// import imageApi from "./imageApi";
+// import locationApi from "./location-api";
+// import notificationApi from "./notification-api";
+// import orderApi from "./order-api";
+// import paymentApi from "./payment-api";
+
+export { authApi };
